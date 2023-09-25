b88ec91b97ac9e5e17f156957ac6e108c3ee0050
Alex Rosa <alexr@gec.inatel.br>
2023-09-25 19:09:12 -0300

    Create the listAdapter

--- app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
+++ app/src/main/java/com/example/busschedule/BusScheduleApplication.kt
@@ -3,6 +3,6 @@ package com.example.busschedule
 import android.app.Application
 import com.example.busschedule.database.AppDatabase
 
-class BusScheduleApplication: Application() {
-    val appDatabase: AppDatabase by lazy { AppDatabase.getDatabase(this)}
+class BusScheduleApplication : Application() {
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
 }
